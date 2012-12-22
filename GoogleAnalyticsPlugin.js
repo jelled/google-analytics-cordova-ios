b57702291c040fb4a888afc38eccc18e2ecc4cde
Benjamin Bjurstrom <ben@jelled.com>
2012-12-22 07:21:19 -0800

    Removed startGA() from GoogleAnalyticsPlugin.js

--- GoogleAnalyticsPlugin.js
+++ GoogleAnalyticsPlugin.js
@@ -1,27 +1,25 @@
-function startGA(){
-    if (!window.GA) {
-        window.GA = {
-            trackerWithTrackingId: function(id) {
-                PhoneGap.exec("GoogleAnalyticsPlugin.trackerWithTrackingId",id);
-                //console.log("trackerWithTrackingId Initialized");
-            },
-            trackView: function(pageUri) {
-                PhoneGap.exec("GoogleAnalyticsPlugin.trackView",pageUri);
-                //console.log("trackView Initialized");
-            },
-            trackEventWithCategory: function(category,action,label,value) {
-                var options = {category:category,
-                    action:action,
-                    label:label,
-                    value:value};
-                PhoneGap.exec("GoogleAnalyticsPlugin.trackEventWithCategory",options);
-            },
-            hitDispatched: function(hitString) {
-                //console.log("hitDispatched :: " + hitString);
-            },
-            trackerDispatchDidComplete: function(count) {
-                //console.log("trackerDispatchDidComplete :: " + count);
-            }
-        }
-    }
+if (!window.GA) {
+	window.GA = {
+		trackerWithTrackingId: function(id) {
+			PhoneGap.exec("GoogleAnalyticsPlugin.trackerWithTrackingId",id);
+			//console.log("trackerWithTrackingId Initialized");
+		},
+		trackView: function(pageUri) {
+			PhoneGap.exec("GoogleAnalyticsPlugin.trackView",pageUri);
+			//console.log("trackView Initialized");
+		},
+		trackEventWithCategory: function(category,action,label,value) {
+			var options = {category:category,
+				action:action,
+				label:label,
+				value:value};
+			PhoneGap.exec("GoogleAnalyticsPlugin.trackEventWithCategory",options);
+		},
+		hitDispatched: function(hitString) {
+			//console.log("hitDispatched :: " + hitString);
+		},
+		trackerDispatchDidComplete: function(count) {
+			//console.log("trackerDispatchDidComplete :: " + count);
+		}
+	}
 }
--- README.md
+++ README.md
@@ -7,14 +7,13 @@ Setup:
 1. Add the SystemConfiguration and CoreData frameworks to Xcode.
 2. Open your Cordova.plist file add a new entry under Plugins with the key as googleAnalyticsPlugin (note the lower case g) and the value as GoogleAnalyticsPlugin. Then under ExternalHosts add a new entry with a value of *
 3. Drag and drop the GoogleAnalytics folder onto your Plugins folder in Xcode. Select 'Copy items into destination group's folder (if needed)', select 'Create groups for any added folders', and check your target under 'Add to targets'.
-4. Reference your cordova.js file and the GoogleAnalyticsPlugin.js in your html. To use, wrap startGA() and any of the window.GA methods inside of an onDeviceReady functions.
+4. Reference your cordova.js file and the GoogleAnalyticsPlugin.js in your html. To use, wrap the window.GA methods inside of an onDeviceReady function.
 
 Example:
 ---------------------
 	<script type="text/javascript">
 		document.addEventListener("deviceready", onDeviceReady, false);
 		function onDeviceReady() {
-			startGA();
 			window.GA.trackerWithTrackingId("UA-XXXXXXXX-X");
 			window.GA.trackView("/index");
 		}
@@ -26,4 +25,4 @@ For a more detailed guide see http://jelled.com/google-analytics-plugin-for-phon
 
 Credit:
 ---------------------
-The updated GoogleAnalyticsPlugin.m and GoogleAnalyticsPlugin.h files were courtesy of nocksf in this discussion: https://groups.google.com/d/msg/phonegap/uqYjTmd4w_E/fZvJCpAbtsUJ
+The updated GoogleAnalyticsPlugin.m, GoogleAnalyticsPlugin.h, and GoogleAnalyticsPlugin.js files were courtesy of nocksf in this discussion: https://groups.google.com/d/msg/phonegap/uqYjTmd4w_E/fZvJCpAbtsUJ
